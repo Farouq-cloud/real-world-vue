0add3d965e749296d6e279833f80f32856fde9ff
Farouq-cloud <farouq.alayaki@gmail.com>
2021-02-25 12:06:36 +0100

    Create EventList.vue

---
+++ src/views/EventList.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="home">
+    <h1>Event for Goods</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from "@/components/EventCard.vue";
+import EventServices from "@/services/EventServices.js";
+
+export default {
+  name: "EventList",
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+      events:null
+    }
+  },
+  created(){
+    EventServices.getEvents()
+    .then(response => {
+      this.events= response.data;
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+
+
+};
+</script>
+
+<style scoped>
+.home{
+display: flex;
+flex-direction: column;
+align-items: center;
+}
+</style>
